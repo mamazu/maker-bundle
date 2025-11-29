91ddb1bdbb63560e9974dc9c596a87a15c831f53
mamazu <14860264+mamazu@users.noreply.github.com>
2025-11-29 14:11:07 +0100

    Add helpful error message on invalid configuration
    When registering the sulu maker bundle you also have to manually register the Symfony maker bundle

--- src/SuluMakerBundle.php
+++ src/SuluMakerBundle.php
@@ -2,8 +2,16 @@
 
 namespace FriendsOfSulu\MakerBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SuluMakerBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        if(!$container->hasExtension('maker')) {
+            throw new \LogicException('The Symfony MakerBundle is not installed or not enabled in the bundles.php file.');
+        }
+        parent::build($container);
+    }
 }
